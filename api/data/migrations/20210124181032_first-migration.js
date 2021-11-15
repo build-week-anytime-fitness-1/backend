7da6f066e8c1fe92a1487565a49592889131c108
rdperry031 <rdperry031@gmail.com>
2021-11-15 09:36:24 -0600

    .....

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,24 +1,39 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('roles', (roles) => {
-      roles.increments('role_id');
-      roles.string('role_name').notNullable().unique();
+    // .createTable('roles', (roles) => {
+    //   roles.increments('role_id');
+    //   roles.string('role_name').notNullable().unique();
+    // })
+    // .createTable('users', (users) => {
+    //   users.increments('user_id');
+    //   users.string('first_name').notNullable();
+    //   users.string('last_name').notNullable();
+    //   users.string('username', 200).notNullable().unique();
+    //   users.string('password', 200).notNullable();
+    //   users.integer('role_id')
+    //     .unsigned()
+    //     .notNullable()
+    //     .references('role_id')
+    //     .inTable('roles')
+    //     .onDelete('RESTRICT')
+    //     .onUpdate('CASCADE')
+    // })
+    .createTable("roles", tbl => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable().unique();
     })
-    .createTable('users', (users) => {
-      users.increments('user_id');
-      users.string('first_name').notNullable();
-      users.string('last_name').notNullable();
-      users.string('username', 200).notNullable().unique();
-      users.string('password', 200).notNullable();
-      users.integer('role_id')
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl.string("username", 128).notNullable().unique();
+      tbl.string("password", 256).notNullable();
+      tbl.integer("role")
         .unsigned()
-        .notNullable()
-        .references('role_id')
-        .inTable('roles')
-        .onDelete('RESTRICT')
-        .onUpdate('CASCADE')
-        
-    })
+        .references("roles.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE")
+        .defaultTo(2);
+    });
+};
       .createTable('classes', (classes) => {
       classes.increments('class_id');
       classes.string('class_name', 200).notNullable();
